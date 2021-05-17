fdeac3ff0e1bb1a43f0a80a4e5b6e614683fbf3c
hatembellaaj <gitlab-mdsoft@mdsoft-int.com>
2021-05-17 13:10:09 +0100

    upload file

--- test.php
+++ test.php
@@ -1,5 +1,25 @@
 <?php
 
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+
+
+
 $wsdl   = 'https://ws.orias.fr/service?wsdl';
 $client = new SoapClient($wsdl, array('trace'=>1));  // The trace param will show you errors
 $user= 'WUU7EOCWD0GX9GHZX6Q5';
